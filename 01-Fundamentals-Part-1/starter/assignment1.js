1947895c197023adec2c477db7de514d46278d83
anaskaysar <kaysarul.apurba@northsouth.edu>
2025-07-15 23:36:45 -0400

    untill variables type

---
+++ 01-Fundamentals-Part-1/starter/assignment1.js
@@ -0,0 +1,16 @@
+// 1.Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+
+let country = "Bangladesh";
+let continent = "Asia";
+let population = "16Cr or 1600 Milions"
+
+console.log(country);
+console.log(continent);
+console.log(population)
+
+// 2.Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+let isIsland = true;
+let language;
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,45 @@
+/*
 let js = "amazing";
-if ((js = "amazing")) alert("JavaScript is Fun");
+// if ((js = "amazing")) alert("JavaScript is Fun");
 
 console.log(40 + 9 + 20 - 22 - 22);
+
+let firstName = ' Bob';
+let lastName = 'Taylor';
+
+
+console.log(firstName)
+
+
+// let new = 27; reserver so cna't be used
+
+let Person = "Anas"; //only used for OOP class names
+
+let PI = 3.1415;   //All capital for constant
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob)
+
+
+let jsIsFun = true;
+console.log(jsIsFun);
+
+console.log(typeof jsIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+
+jsIsFun = "Yes"
+console.log(typeof jsIsFun);
+let year;
+console.log(year);
+console.log(typeof year);
+
+
+year = 2025;
+console.log(year)
+console.log(typeof year)
+
+*/
