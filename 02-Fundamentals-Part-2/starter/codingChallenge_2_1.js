9597810a569b95d6b4c304e2302bf3eed74b4cd8
anaskaysar <kaysarul.apurba@northsouth.edu>
2025-07-18 08:16:06 -0400

    Part-2-Challenge 1

--- 02-Fundamentals-Part-2/starter/codingChallenge_2_1.js
+++ 02-Fundamentals-Part-2/starter/codingChallenge_2_1.js
@@ -1,1 +1,44 @@
 'use strict'
+/*
+CHALLENGE #1
+Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new gymnastics discipline, which works differently.
+
+Each team competes 3 times, and then the average of the 3 scores is calculated (so one average score per team).
+
+A team only wins if it has at least double the average score of the other team. Otherwise, no team wins!
+
+TEST DATA 1: Dolphins scored 44, 23, and 71. Koalas scored 65, 54, and 49.
+
+TEST DATA 2: Dolphins scored 85, 54, and 41. Koalas scored 23, 34, and 27.
+
+*/
+/* Write your code below. Good luck! 🙂 */
+
+/*
+Create an arrow function calcAverage to calculate the average of 3 scores. This function should have three parameters and return a single number (the average score).
+*/
+
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+/*
+Create two new variables — scoreDolphins and scoreKoalas, and assign the value returned from the calcAverage function to them (you will need to call this function, and pass scores as arguments). 
+*/
+
+// const scoreDolphins = calcAverage(44, 23, 71);
+// const scoreKoalas = calcAverage(65, 54, 49);
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage(23, 34, 27);
+console.log(scoreDolphins, scoreKoalas);
+
+
+const checkWinner = (avgDolphins, avgKoalas) => {
+    if (avgDolphins >= (avgKoalas * 2)) {
+        console.log(`Dolphins win (${avgDolphins} vs. ${(avgKoalas)})`)
+    } else if (avgKoalas >= (avgDolphins * 2)) {
+        console.log(`Koalas win (${avgKoalas} vs. ${(avgDolphins)})`)
+    } else {
+        console.log("No team wins..")
+    }
+    return;
+}
+
+checkWinner(scoreDolphins, scoreKoalas);
